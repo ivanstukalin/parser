2407bbc0425afdd6d118f5195636845c329ef765
Timur Kadyrov <sornowy@gmail.com>
2025-03-28 04:17:25 +0700

    add index

--- migrations/20230321120001_create_new_domains_table.sql
+++ migrations/20230321120001_create_new_domains_table.sql
@@ -6,10 +6,15 @@ CREATE TABLE IF NOT EXISTS cryptocurrencies (
     created_at TIMESTAMP DEFAULT NOW()
 );
 
+CREATE INDEX IF NOT EXISTS idx_cryptocurrencies_name ON cryptocurrencies(name);
+CREATE INDEX IF NOT EXISTS idx_cryptocurrencies_code ON cryptocurrencies(code);
+
 INSERT INTO cryptocurrencies (name, code) VALUES
 ('USDT', 'USDT'),
 ('Bitcoin', 'BTC'),
 ('Ethereum', 'ETH');
 
 -- migrate:down
+DROP INDEX IF EXISTS idx_cryptocurrencies_code;
+DROP INDEX IF EXISTS idx_cryptocurrencies_name;
 DROP TABLE cryptocurrencies;
